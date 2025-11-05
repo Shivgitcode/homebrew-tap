a61e0d9eed4bd5482b63c2846e366b628e857ab7
Shivansh Agarwal <shivneeraj2004@gmail.com>
2025-11-05 13:05:47 +0530

    Brew formula update for qv version v2.0.4

--- Formula/qv.rb
+++ Formula/qv.rb
@@ -5,21 +5,21 @@
 class Qv < Formula
   desc "A CLI tool for quick variable management"
   homepage "https://github.com/shivgitcode/qv"
-  version "2.0.3"
+  version "2.0.4"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/shivgitcode/qv/releases/download/v2.0.3/qv_2.0.3_darwin_amd64.tar.gz"
-      sha256 "398b86f93beb15b5ce5c4a0a358b3089937f7ae1b952079bc9ce8f4ff1c2142a"
+      url "https://github.com/shivgitcode/qv/releases/download/v2.0.4/qv_2.0.4_darwin_amd64.tar.gz"
+      sha256 "1aaefa97c24eb873818c9db12726d90322840eff42449c22d6c575a088600406"
 
       def install
         bin.install "qv"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/shivgitcode/qv/releases/download/v2.0.3/qv_2.0.3_darwin_arm64.tar.gz"
-      sha256 "452315fd869817aa1fd36210bbadc0b2e0967e6e7a248a4aed395770201f93fd"
+      url "https://github.com/shivgitcode/qv/releases/download/v2.0.4/qv_2.0.4_darwin_arm64.tar.gz"
+      sha256 "757197ef06e68b9683d89b2511b551e2b5a6897ef40f9257f5241ecc389cd6c0"
 
       def install
         bin.install "qv"
@@ -29,15 +29,15 @@ class Qv < Formula
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/shivgitcode/qv/releases/download/v2.0.3/qv_2.0.3_linux_amd64.tar.gz"
-      sha256 "a12e80609bab7deff6c1a7f7b07af9e96f7dd705acba8fa39a199a784bfad6b5"
+      url "https://github.com/shivgitcode/qv/releases/download/v2.0.4/qv_2.0.4_linux_amd64.tar.gz"
+      sha256 "f5414f989d2406ed29cdf5b382dbcba3b789a8281aaeb2abfbfa524e8fffcf8b"
       def install
         bin.install "qv"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/shivgitcode/qv/releases/download/v2.0.3/qv_2.0.3_linux_arm64.tar.gz"
-      sha256 "70c9e3b92d7c9ac7c069701d8b41ef01f7b02304c10c53d89f870acaf50929ae"
+      url "https://github.com/shivgitcode/qv/releases/download/v2.0.4/qv_2.0.4_linux_arm64.tar.gz"
+      sha256 "cb7d08b8018ddea735c8ff91ee728a4e7838ac6598dcb430cd95a885a6fffafd"
       def install
         bin.install "qv"
       end
@@ -45,6 +45,6 @@ class Qv < Formula
   end
 
   test do
-    system "#{bin}/qv", "list"
+    system "#{bin}/qv"
   end
 end
